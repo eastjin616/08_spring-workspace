12e4b768d713ec2971fe9c7908baa25e44694c5d
seo <djseo9812@gmail.com>
2025-05-02 10:12:58 +0900

    123

---
+++ PublicDataUse/src/main/java/com/kh/opendata/controller/APIController.java
@@ -0,0 +1,43 @@
+package com.kh.opendata.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class APIController {
+
+	private static final String serviceKey = "VSgXc34Vs94ijfKVTpeJr85n70S9Xq9bY8PLyw9wCrGAzraqE4Gj6ecSZ88ZSiuSvN3JC1aXQZ0NVZDGuuugoA%3D%3D";
+
+	@ResponseBody
+	@RequestMapping(value = "air.do", produces ="application/json; charset=utf-8")
+	public String airPollution(String location) throws IOException {
+		String url = "https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty";
+		url += "?serviceKey=" + serviceKey;
+		url += "&sidoName=" + URLEncoder.encode(location,"utf-8");
+		url += "&returnType=json";
+		
+		URL requestUrl = new URL(url);
+		HttpURLConnection urlConnection = (HttpURLConnection)requestUrl.openConnection();
+		urlConnection.setRequestMethod("GET");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader( urlConnection.getInputStream()));
+		
+		String responseText = "";
+		String line;
+		while((line=br.readLine()) != null) {
+			responseText += line;
+		}
+		
+		br.close();
+		urlConnection.disconnect();
+
+		return responseText;
+
+	}
+}
--- openDataProject/src/main/java/com/kh/opendata/run/AirPollutionJavaApp.java
+++ openDataProject/src/main/java/com/kh/opendata/run/AirPollutionJavaApp.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -17,29 +16,61 @@ import com.kh.opendata.model.vo.AirVo;
 
 public class AirPollutionJavaApp {
 
-   // 발급받은 인증키 정보 변수에 담아두기
-   public static final String servicekey = "U02uX7TWYMipQZdwIgIXFCBgiweQj4IhyqFruKsW2XtPQ0JwfBtrr7SNOODQnPcZK1K8q%2BoK4htDcCcYdGhnwQ%3D%3D";
-   
-   public static void main(String[] args) throws IOException {
+	public static final String servicekey = "VSgXc34Vs94ijfKVTpeJr85n70S9Xq9bY8PLyw9wCrGAzraqE4Gj6ecSZ88ZSiuSvN3JC1aXQZ0NVZDGuuugoA%3D%3D";
+	public static void main(String[] args) throws IOException {
+		
+		String url = "https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty";
+		url += "?servicekey=" + servicekey;
+		url += "&sidoName="+ URLEncoder.encode("서울", "UTF-8");
+		url += "&returnType=json";
+		
+		URL requestUrl = new URL(url);
+		
+		HttpURLConnection urlConnection = (HttpURLConnection)requestUrl.openConnection();
+		
+		urlConnection.setRequestMethod("GET");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+		
+		String responseText = "";
+		String line;
+		while((line = br.readLine())!=null) {
+			responseText += line;
+		}
+		System.out.println("응답 데이터 : " + responseText);
+		
+		JsonObject totalObj = JsonParser.parseString(responseText).getAsJsonObject();
+		JsonObject responseObj = totalObj.getAsJsonObject("response");
+		JsonObject bodyObj = responseObj.getAsJsonObject("body");
+		
+		int totalCount = bodyObj.get("totalCount").getAsInt();
+		JsonArray itemaArr = bodyObj.getAsJsonArray("items");
+		
+		ArrayList<AirVo> list = new ArrayList<AirVo>();
+		
+		for(int i=0; i<itemaArr.size(); i++) {
+			JsonObject item = itemaArr.get(i).getAsJsonObject();
+			
+			AirVo air = new AirVo();
+			air.setStationName(item.get("stationName").getAsString());
+			  air.setDataTime(item.get("dataTime").getAsString());
+			  air.setKhaiValue(item.get("khaiValue").getAsString());
+			  air.setPm10Value(item.get("pm10Value").getAsString());
+			  air.setSo2Value(item.get("so2Value").getAsString());
+			  air.setCoValue(item.get("coValue").getAsString());
+			  air.setNo2Value(item.get("no2Value").getAsString());
+			  air.setO3Value(item.get("o3Value").getAsString());
+			  
+			  list.add(air);
+		}
+	
+		for(AirVo a : list) {
+			System.out.println(a);
+		}
+		br.close();
+		urlConnection.disconnect();
+	
+	
+	}
 
-      
-      // OPEN API 서버로 요청하고자 하는 URL 만들기
-      String url = "https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty";
-      url += "?serviceKey="+servicekey;
-      url += "&sidoName="+URLEncoder.encode("서울", "UTF-8");
-      url += "&returnType=json";
-      
-      URL requestUrl = new URL(url);
-      HttpURLConnection urlConnection = (HttpURLConnection)requestUrl.openConnection();
-      urlConnection.setRequestMethod("GET");
-      
-      BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-      String line = null;
-      while((line = br.readLine()) != null) {
-    	  System.out.println(line);
-      }
-      
-      br.close();
-      urlConnection.disconnect();
-   }
 }
